eb8601d2f23d30477012689d3c2de85acb85daac
lukas <lukas.walisch@hotmail.com>
2016-01-05 12:03:31 +0100

    JFileChooser added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by lukas on 05.01.2016.
  */
@@ -5,6 +12,11 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("hello folks!");
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.showOpenDialog(null);
+        File file = fileChooser.getSelectedFile();
+        System.out.println(file.getName());
+
+        }
     }
-}
+
