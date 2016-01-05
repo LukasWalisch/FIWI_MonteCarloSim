1b97211269dd951ec3c977a0012e813103917547
lukas <lukas.walisch@hotmail.com>
2016-01-05 18:19:31 +0100

    added jcommon library

---
+++ src/LineChart.java
@@ -0,0 +1,10 @@
+import org.jfree.ui.ApplicationFrame;
+
+/**
+ * Created by lukas on 05.01.2016.
+ */
+public class LineChart extends ApplicationFrame {
+    public LineChart(String title) {
+        super(title);
+    }
+}
