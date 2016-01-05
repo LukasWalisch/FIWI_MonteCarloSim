2d7fdb62eae765c5a138ee96a24a8c2b1341e73d
lukas <lukas.walisch@hotmail.com>
2016-01-05 12:18:27 +0100

    JFileChooser added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,10 +14,30 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         JFileChooser fileChooser = new JFileChooser();
-        fileChooser.showOpenDialog(null);
-        File file = fileChooser.getSelectedFile();
-        System.out.println(file.getName());
+        fileChooser.setFileFilter(new FileFilter() {
+            @Override
+            public boolean accept(File f) {
+                if (f.isDirectory())
+                    return true;
+                else if (f.getName().endsWith(".csv"))
+                    return true;
+                else
+                    return false;
+            }
 
+            @Override
+            public String getDescription() {
+                return "*.csv";
+            }
+        });
+        try {
+            fileChooser.showOpenDialog(null);
+            File file = fileChooser.getSelectedFile();
+            System.out.println(file.getName());
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            return;
         }
     }
+}
 
