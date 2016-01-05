56512babe7cb158e259f3b2a4c7da9947a679fb0
Richard <richard22412@hotmail.com>
2016-01-05 18:39:09 +0100

    SomeChanges

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,7 +7,15 @@ import model.MonteCarlo;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        startGUI startGUI = new startGUI();
+        //    startGUI startGUI = new startGUI();
+        Basket basket=new Basket();
+        basket.setStartValue(1000.0);
+        basket.setComputedMu(0.05);
+        basket.setComputedSigma(0.15);
+        basket.setDays(100);
+        MonteCarlo monteCarlo=new MonteCarlo(basket);
+        monteCarlo.alg();
+
     }
 }
 
